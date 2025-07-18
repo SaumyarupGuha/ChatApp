8244cc657ff76e17501742686ee71cd5e7904ac5
SaumyarupGuha <guhasaumyarup100@gmail.com>
2025-07-19 00:42:29 +0530

    Updated LoginPage and ProfilePage, excluded node_modules

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -10,6 +10,14 @@ const LoginPage = (selectedUser) => {
   const[bio, setBio] = useState("")
   const[isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const onSubmitHandler =(event)=>{
+    event.preventDefault();
+
+    if(currState === 'Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true)
+    }
+  }
+
   return  (
     <div className='min-h-screen bg-cover bg-center flex items-center 
     justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
@@ -18,11 +26,14 @@ const LoginPage = (selectedUser) => {
     <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
       
     {/* ---- right ----*/}
-    <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex 
+    <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex 
     flex-col gap-6 rounded-lg shadow-lg'>
       <h2>
         {currState}
-        <img src={assets.arrow_icon} alt="" className='w-5 cursor-poiter'/>
+        {isDataSubmitted && 
+        <img src={assets.arrow_icon} onClick={()=>setIsDataSubmitted(false)} alt="" className='w-5 cursor-poiter'/>
+        }
+        
       </h2>
 
       {currState === "Sign up" && !isDataSubmitted && (
@@ -43,11 +54,34 @@ const LoginPage = (selectedUser) => {
 
       {
         currState === "Sign up" && isDataSubmitted && (
-          <textarea rows={4} className='p-2 border border-gray-500 rounded-md 
-          focus:outline-none focus:ring-2 focus:ring-indigo-500' />
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} rows={4}
+           className='p-2 border border-gray-500 rounded-md 
+          focus:outline-none focus:ring-2 focus:ring-indigo-500' 
+          placeholder='Provide a short bio...' required/>
         )
       }
 
+      <button className='py-3 bg-gradient-to-r from-purple-400
+       to-violet-600 text-white rounded-md cursor-pointer'>
+        {currState === "Sign up" ? "Create Account": "Login Now"}
+      </button>
+
+      <div className='flex items-center gap-2 text-sm text-gray-500'>
+        <input type="checkbox" required/>
+        <p>Agree to the terms of use & privacy policy.</p>
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        {currState === "Sign up" ? (
+          <p className='text-sm text-gray-600'>Already have an account?
+          <span onClick={()=>{setCurrState("Login"); isDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+         ) : (
+            <p className='text-sm text-gray-600'>Create an Account
+            <span onClick={()=>setCurrState("Sign up")}className='font-medium text-violet-500 cursor-pointer'>Click Here
+            </span></p>
+          )
+        }
+      </div>
 
     </form>
     </div>
